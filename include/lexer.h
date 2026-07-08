f14ee009d72f6535e1b99a8e5970727ddb3c5e64
no-0-name <87208495+no-0-name@users.noreply.github.com>
2026-07-08 12:58:54 +0300

    Delete comments from lexer.h

--- include/lexer.h
+++ include/lexer.h
@@ -5,22 +5,22 @@
 
 typedef enum {
     T_EOF, T_NUMBER, T_STRING, T_IDENT,
-    T_INSERT_INTO, //
-    T_ROLEPLAY, //
-    T_HARDER_IF, //
-    T_OR_IM_GONNA_CUM, //
-    T_RIDE_UNTIL, //
-    T_FEMBOY_HOURS, //
-    T_STOP_RIDING,  //
-    T_KEEP_RIDING,  //
-    T_CUMING,  //
-    T_MOAN, //
-    T_TRY_ME,  //
-    T_CATCH_ME,  //
-    T_THROW_ME, //
-    T_OH_YES,            //
-    T_NOT_THERE,   //
-    T_EMPTY_BALLS,  //
+    T_INSERT_INTO,
+    T_ROLEPLAY,
+    T_HARDER_IF,
+    T_OR_IM_GONNA_CUM,
+    T_RIDE_UNTIL,
+    T_FEMBOY_HOURS,
+    T_STOP_RIDING,
+    T_KEEP_RIDING,
+    T_CUMING,
+    T_MOAN,
+    T_TRY_ME,
+    T_CATCH_ME,
+    T_THROW_ME,
+    T_OH_YES,
+    T_NOT_THERE,
+    T_EMPTY_BALLS,
     T_PLUS, 
     T_MINUS, 
     T_STAR, 
